2627301632840e72784f8fac27368333f5e2c8d2
rajinipreethajohn <rajinipreethaj@hotmail.com>
2024-01-25 14:34:37 +0800

    Validating exception handling

--- src/exception.py
+++ src/exception.py
@@ -1,17 +1,25 @@
 import sys
+import logging
 
-def error_message_detail(error, sys):
-    _,_,exc_tb= sys.exc_info()
+
+logging.basicConfig(filename='logs/error.log', level=logging.INFO, format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s')
+
+
+logger = logging.getLogger('src.logger')
+
+
+
+def error_message_detail(error, sys_info):
+    _,_,exc_tb= sys_info
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error has occured while processing the python script named [{0}] in line number [{1}] error message [{2}] .".format()
-    file_name,  exc_tb.tb_lineno, str(error)
-    
+    error_message="Error has occured while processing the python script named [{0}] in line number [{1}] error message [{2}] .".format(
+    file_name,  exc_tb.tb_lineno, str(error))
     return error_message
     
 class CustomException(Exception):
-    def __init__(self,error_message,sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, sys)
+    def __init__(self,error,sys_info):
+        super().__init__(str(error))
+        self.error_message = error_message_detail(error, sys_info)
         
     def __str__(self):
         return self.error_message
